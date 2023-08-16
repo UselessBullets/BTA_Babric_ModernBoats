8997a8d41908d30e1094928af865b508fee7bbde
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-15 19:11:47 -0500

    Update ModernBoats.java

--- src/main/java/useless/modernboats/ModernBoats.java
+++ src/main/java/useless/modernboats/ModernBoats.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+
 public class ModernBoats implements ModInitializer {
     public static final String MOD_ID = "modernboats";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
